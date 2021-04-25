947f65062cb56cb41dbac86276ce1a224c1a708f
Bryan Civils <hermanbciv@gmail.com>
2021-04-25 02:30:19 -0400

    Major update to logic for quiz question displays and time function, and minor update to styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+var head = document.querySelector(".head")
+var timer = document.querySelector("#clock");
 var mainSection = document.querySelector(".primary-content-container");
 var welcome = document.querySelector("#welcome");
 var startBttn = document.querySelector("#start-bttn");
@@ -20,9 +22,10 @@ questionEvalContainer.setAttribute("class", "question-eval-container");
 questionEval.setAttribute("class", "question-eval");
 
 var timerCount = 75
+head.children[0].children[0].appendChild(timer);
+timer.textContent = timerCount
 
-
-
+var quizDefinitions
 
 startBttn.addEventListener("click", function() {
     welcome.style.display = "none";
@@ -41,7 +44,7 @@ function randQuestion() {
         
 } */
 
-function quizQuestionConstructor() {
+/* function quizQuestionConstructor() {
     var quizDefinitions = [
         {
             quizQuestion:  "Which of the following is not a county in North Carolina?",
@@ -110,4 +113,208 @@ function quizQuestionConstructor() {
             }
         });
     });
-}};
+}}; */
+
+function quizQuestionConstructor(event) {
+    // event.preventDefault();
+    /*
+    var quizDefinitions = [
+        {
+            quizQuestion:  "Which of the following is not a county in North Carolina?",
+            qA: "Clay County",
+            qB: "Cleveland County",
+            qC: "Ashe County",
+            qD: "Greenville County",
+            answer:  "Greenville County"
+        },
+        {
+            quizQuestion:  "In 2019 North Carolina ranked number one in the United States for the production of which crop:",
+            qA: "A.  strawberries",
+            qB: "B.  peanuts",
+            qC: "C.  tobacco",
+            qD: "D.  sweet potatoes",
+            answer:  "C.  tobacco"
+        },
+        {
+            quizQuestion: "Which of the following famous jazz musicians was not born in North Carolina?",
+            qA: "A.  John Coltrane",
+            qB: "B.  Nina Simone",
+            qC: "C.  Dizzy Gillespie",
+            qD: "D.  Thelonious Monk",
+            answer: "C.  Dizzy Gillespie"
+        }
+    ];
+
+    mainSection.appendChild(containerPrimary);
+    containerPrimary.appendChild(quizQuestionHead);
+    containerPrimary.appendChild(quizQuestionListItemA)
+    quizQuestionListItemA.appendChild(quizQuestionListItemABttn);
+    containerPrimary.appendChild(quizQuestionListItemB);
+    quizQuestionListItemB.appendChild(quizQuestionListItemBBttn);
+    containerPrimary.appendChild(quizQuestionListItemC);
+    quizQuestionListItemC.appendChild(quizQuestionListItemCBttn);
+    containerPrimary.appendChild(quizQuestionListItemD);
+    quizQuestionListItemD.appendChild(quizQuestionListItemDBttn);
+    containerPrimary.appendChild(questionEvalContainer);
+    questionEvalContainer.appendChild(questionEval);
+    */     
+    
+    /* for (let i = 0; i < quizDefinitions.length; i++) {
+        quizQuestionHead.textContent = quizDefinitions[i]["quizQuestion"];
+        quizQuestionListItemABttn.textContent = quizDefinitions[i]["qA"];
+        quizQuestionListItemBBttn.textContent = quizDefinitions[i]["qB"];
+        quizQuestionListItemCBttn.textContent = quizDefinitions[i]["qC"];
+        quizQuestionListItemDBttn.textContent = quizDefinitions[i]["qD"];
+
+        quizQuestionListItemABttn.addEventListener("click", function() {
+            console.log(`TEST | Button A has been clicked`)
+            console.log(`TEST | THE ANSWSER IS:  ${quizDefinitions[i]["answer"]}`)
+            if (quizQuestionListItemABttn === quizDefinitions[i]["answer"]) {
+                questionEval.textContent = "CORRECT";
+            } else {
+                questionEval.textContent = "INCORRECT";
+            }
+        });
+        quizQuestionListItemBBttn.addEventListener("click", function() {
+            console.log(`TEST | Button B has been clicked`)
+            console.log(`TEST | THE ANSWSER IS:  ${quizDefinitions[i]["answer"]}`)
+            if (quizQuestionListItemBBttn === quizDefinitions[i]["answer"]) {
+                questionEval.textContent = "CORRECT";
+            } else {
+                questionEval.textContent = "INCORRECT";
+            }
+        });
+        quizQuestionListItemCBttn.addEventListener("click", function() {
+            console.log(`TEST | Button C has been clicked`)
+            console.log(`TEST | THE ANSWSER IS:  ${quizDefinitions[i]["answer"]}`)
+            if (quizQuestionListItemCBttn === quizDefinitions[i]["answer"]) {
+                questionEval.textContent = "CORRECT";
+            } else {
+                questionEval.textContent = "INCORRECT";
+            }
+        });
+        quizQuestionListItemDBttn.addEventListener("click", function() {
+            console.log(`TEST | Button D has been clicked`)
+            console.log(`TEST | THE ANSWSER IS:  ${quizDefinitions[i]["answer"]}`)
+            if (quizQuestionListItemDBttn === quizDefinitions[i]["answer"]) {
+                questionEval.textContent = "CORRECT";
+            } else {
+                questionEval.textContent = "INCORRECT";
+            }
+        });
+/////////////// Q1 ///////////////
+    } // this is the closing for the 'for (let i = 0; ...)' loop */
+
+    /*
+    quizQuestionHead.textContent = quizDefinitions[0]["quizQuestion"];
+    quizQuestionListItemABttn.textContent = quizDefinitions[0]["qA"];
+    quizQuestionListItemBBttn.textContent = quizDefinitions[0]["qB"];
+    quizQuestionListItemCBttn.textContent = quizDefinitions[0]["qC"];
+    quizQuestionListItemDBttn.textContent = quizDefinitions[0]["qD"];
+    */
+   quizText(0);
+
+   /*
+    quizQuestionListItemABttn.addEventListener("click", function() {
+        console.log(`TEST | Button A has been clicked`)
+        console.log(`TEST | THE ANSWSER IS:  ${quizDefinitions[0]["answer"]}`)
+        if (quizQuestionListItemABttn === quizDefinitions[0]["answer"]) {
+            questionEval.textContent = "CORRECT";
+        } else {
+            questionEval.textContent = "INCORRECT";
+        }
+    });
+    quizQuestionListItemBBttn.addEventListener("click", function() {
+        console.log(`TEST | Button B has been clicked`)
+        console.log(`TEST | THE ANSWSER IS:  ${quizDefinitions[0]["answer"]}`)
+        if (quizQuestionListItemBBttn === quizDefinitions[0]["answer"]) {
+            questionEval.textContent = "CORRECT";
+        } else {
+            questionEval.textContent = "INCORRECT";
+        }
+    });
+    quizQuestionListItemCBttn.addEventListener("click", function() {
+        console.log(`TEST | Button C has been clicked`)
+        console.log(`TEST | THE ANSWSER IS:  ${quizDefinitions[0]["answer"]}`)
+        if (quizQuestionListItemCBttn === quizDefinitions[0]["answer"]) {
+            questionEval.textContent = "CORRECT";
+        } else {
+            questionEval.textContent = "INCORRECT";
+        }
+    });
+    quizQuestionListItemDBttn.addEventListener("click", function() {
+        console.log(`TEST | Button D has been clicked`)
+        console.log(`TEST | THE ANSWSER IS:  ${quizDefinitions[0]["answer"]}`)
+        if (quizQuestionListItemDBttn === quizDefinitions[0]["answer"]) {
+            questionEval.textContent = "CORRECT";
+        } else {
+            questionEval.textContent = "INCORRECT";
+            }
+    });
+    */
+   quizEval(0);
+
+
+} // this is the closing for the quizQuestionConstructor function
+
+function quizText(index) {
+    quizDefinitions = [
+        {
+            quizQuestion:  "Which of the following is not a county in North Carolina?",
+            qA: "Clay County",
+            qB: "Cleveland County",
+            qC: "Ashe County",
+            qD: "Greenville County",
+            answer:  "Greenville County"
+        },
+        {
+            quizQuestion:  "In 2019 North Carolina ranked number one in the United States for the production of which crop:",
+            qA: "A.  strawberries",
+            qB: "B.  peanuts",
+            qC: "C.  tobacco",
+            qD: "D.  sweet potatoes",
+            answer:  "C.  tobacco"
+        },
+        {
+            quizQuestion: "Which of the following famous jazz musicians was not born in North Carolina?",
+            qA: "A.  John Coltrane",
+            qB: "B.  Nina Simone",
+            qC: "C.  Dizzy Gillespie",
+            qD: "D.  Thelonious Monk",
+            answer: "C.  Dizzy Gillespie"
+        }
+    ];
+
+    mainSection.appendChild(containerPrimary);
+    containerPrimary.appendChild(quizQuestionHead);
+    containerPrimary.appendChild(quizQuestionListItemA)
+    quizQuestionListItemA.appendChild(quizQuestionListItemABttn);
+    containerPrimary.appendChild(quizQuestionListItemB);
+    quizQuestionListItemB.appendChild(quizQuestionListItemBBttn);
+    containerPrimary.appendChild(quizQuestionListItemC);
+    quizQuestionListItemC.appendChild(quizQuestionListItemCBttn);
+    containerPrimary.appendChild(quizQuestionListItemD);
+    quizQuestionListItemD.appendChild(quizQuestionListItemDBttn);
+    containerPrimary.appendChild(questionEvalContainer);
+    questionEvalContainer.appendChild(questionEval);
+
+    quizQuestionHead.textContent = quizDefinitions[index]["quizQuestion"];
+    quizQuestionListItemABttn.textContent = quizDefinitions[index]["qA"];
+    quizQuestionListItemBBttn.textContent = quizDefinitions[index]["qB"];
+    quizQuestionListItemCBttn.textContent = quizDefinitions[index]["qC"];
+    quizQuestionListItemDBttn.textContent = quizDefinitions[index]["qD"];
+};
+
+function quizEval(index) {
+    quizQuestionListItemABttn.addEventListener("click", function() {
+        console.log(`TEST | Button A has been clicked`)
+        console.log(`TEST | THE ANSWSER IS:  ${quizDefinitions[index]["answer"]}`)
+        if (quizQuestionListItemABttn === quizDefinitions[index]["answer"]) {
+            questionEval.textContent = "CORRECT";
+        } else {
+            questionEval.textContent = "INCORRECT";
+            timerCount = timerCount - 10;
+            timer.textContent = timerCount;
+        }
+    })
+};
