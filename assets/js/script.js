7c5b39e71e286adfeb8ca81a0ae3b78e35ddf912
Bryan Civils <hermanbciv@gmail.com>
2021-04-27 00:34:21 -0400

    Update question-to-question functionality and question evaluation display timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,11 +25,44 @@ var timerCount = 75
 head.children[0].children[0].appendChild(timer);
 timer.textContent = timerCount
 
-var quizDefinitions
+/*
+var quizDefinitions = [
+    {
+        quizQuestion:  "Which of the following is not a county in North Carolina?",
+        qA: "Clay County",
+        qB: "Cleveland County",
+        qC: "Ashe County",
+        qD: "Greenville County",
+        answer:  "Greenville County"
+    },
+    {
+        quizQuestion:  "In 2019 North Carolina ranked number one in the United States for the production of which crop:",
+        qA: "A.  strawberries",
+        qB: "B.  peanuts",
+        qC: "C.  tobacco",
+        qD: "D.  sweet potatoes",
+        answer:  "C.  tobacco"
+    },
+    {
+        quizQuestion: "Which of the following famous jazz musicians was not born in North Carolina?",
+        qA: "A.  John Coltrane",
+        qB: "B.  Nina Simone",
+        qC: "C.  Dizzy Gillespie",
+        qD: "D.  Thelonious Monk",
+        answer: "C.  Dizzy Gillespie"
+    }
+];
+*/
+
+var quizQuestionNum = 0
 
 startBttn.addEventListener("click", function() {
     welcome.style.display = "none";
     quizQuestionConstructor();
+    // quizEval(quizQuestionNum);
+    // for (var i = 0; i < quizDefinitions.length; i++) {
+        // quizEval(quizQuestionNum);
+    // }
 })
 
 /*
@@ -44,7 +77,8 @@ function randQuestion() {
         
 } */
 
-/* function quizQuestionConstructor() {
+/*
+function quizQuestionConstructor() {
     var quizDefinitions = [
         {
             quizQuestion:  "Which of the following is not a county in North Carolina?",
@@ -113,9 +147,13 @@ function randQuestion() {
             }
         });
     });
-}}; */
+}};
+*/
 
 function quizQuestionConstructor(event) {
+    console.log(`TEST | start of function quizQuestionConstructor`)
+    // questionEval.style.display = "none";
+    // console.log(`TEST | questionEval.display = 'none' attempt | 1`)
     // event.preventDefault();
     /*
     var quizDefinitions = [
@@ -212,7 +250,7 @@ function quizQuestionConstructor(event) {
     quizQuestionListItemCBttn.textContent = quizDefinitions[0]["qC"];
     quizQuestionListItemDBttn.textContent = quizDefinitions[0]["qD"];
     */
-   quizText(0);
+   quizText(quizQuestionNum);
 
    /*
     quizQuestionListItemABttn.addEventListener("click", function() {
@@ -256,7 +294,8 @@ function quizQuestionConstructor(event) {
        //quizText(index);
        //quizEval(index);
    //}
-   quizEval(0);
+    quizEval(quizQuestionNum);
+
 
    // quizText(1);
    // quizEval(1);
@@ -264,6 +303,7 @@ function quizQuestionConstructor(event) {
 } // this is the closing for the quizQuestionConstructor function
 
 function quizText(index) {
+    
     quizDefinitions = [
         {
             quizQuestion:  "Which of the following is not a county in North Carolina?",
@@ -290,6 +330,7 @@ function quizText(index) {
             answer: "C.  Dizzy Gillespie"
         }
     ];
+    
 
     mainSection.appendChild(containerPrimary);
     containerPrimary.appendChild(quizQuestionHead);
@@ -311,16 +352,65 @@ function quizText(index) {
     quizQuestionListItemDBttn.textContent = quizDefinitions[index]["qD"];
 };
 
+
+function evalDisplayClock() {
+    var evalDisplayInterval = setInterval( function() {
+        evalDisplayTime--
+        console.log(`TEST | evalDisplayTimer clock is running!`)
+
+        if (evalDisplayTime === 0) {
+            clearInterval(evalDisplayInterval);
+            questionEval.style.display = "none";
+            console.log(`TEST | evalDisplayTimer clock has run out!`)
+        }
+    }, 1000);
+}
+
 function quizEval(index) {
+    // quizText(quizQuestionNum);
+    console.log(`TEST | Function quizText has run`);
+    // questionEval.display = "none";
+    // console.log(`TEST | questionEval.display = 'none' attempt | 2`)
+    evalDisplayTime = 2;
     quizQuestionListItemABttn.addEventListener("click", function() {
         console.log(`TEST | Button A has been clicked`)
         console.log(`TEST | THE ANSWSER IS:  ${quizDefinitions[index]["answer"]}`)
         if (quizQuestionListItemABttn === quizDefinitions[index]["answer"]) {
             questionEval.textContent = "CORRECT";
+            console.log(`TEST | if portion of if...then statement has been executed`)
+            // quizQuestionNum++
         } else {
             questionEval.textContent = "INCORRECT";
+            console.log(`TEST | else portion of the if...then statement has been executed`)
             timerCount = timerCount - 10;
             timer.textContent = timerCount;
+            // quizQuestionNum++
         }
+        evalDisplayClock();
+        quizQuestionNum++;
+        console.log(`TEST | quizQuestionNum:  ${quizQuestionNum}`);
+        quizQuestionConstructor();
     })
 };
+
+/*
+function quizEval(index) {
+    quizText(quizQuestionNum);
+    for (var i = 0; i < quizDefinitions.length; i++) {
+        quizQuestionListItemABttn.addEventListener("click", function() {
+            console.log(`TEST | Button A has been clicked`)
+            console.log(`TEST | THE ANSWSER IS:  ${quizDefinitions[index]["answer"]}`)
+            if (quizQuestionListItemABttn === quizDefinitions[index]["answer"]) {
+                questionEval.textContent = "CORRECT";
+                // quizQuestionNum++
+            } else {
+                questionEval.textContent = "INCORRECT";
+                timerCount = timerCount - 10;
+                timer.textContent = timerCount;
+                // quizQuestionNum++
+            }
+            quizQuestionNum++
+        })
+    }
+}
+*/
